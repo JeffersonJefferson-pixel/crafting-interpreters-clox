98ee830d79e97b37650a45f4502e669917d5db83
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2025-08-02 15:53:14 +0700

    add value to nan boxing
    handle nan boxing in print values and values equal function

--- value.c
+++ value.c
@@ -6,14 +6,21 @@
 #include "value.h"
 
 bool valuesEqual(Value a, Value b) {
-    if (a.type != b.type) return false;
-    switch (a.type) {
-        case VAL_BOOL: return AS_BOOL(a) == AS_BOOL(b);
-        case VAL_NIL: return true;
-        case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
-        case VAL_OBJ: return AS_OBJ(a) == AS_OBJ(b);
-        default: return false; // unreachable
-    }
+    #ifdef NAN_BOXING
+        if (IS_NUMBER(a) && IS_NUMBER(b)) {
+            return AS_NUMBER(a) == AS_NUMBER(b);
+        }
+        return a == b;
+    #else
+        if (a.type != b.type) return false;
+        switch (a.type) {
+            case VAL_BOOL: return AS_BOOL(a) == AS_BOOL(b);
+            case VAL_NIL: return true;
+            case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+            case VAL_OBJ: return AS_OBJ(a) == AS_OBJ(b);
+            default: return false; // unreachable
+        }
+    #endif
 }
 
 void initValueArray(ValueArray* array) {
@@ -39,12 +46,24 @@ void freeValueArray(ValueArray* array) {
 }
 
 void printValue(Value value) {
-    switch (value.type) {
-        case VAL_BOOL:
+    #ifdef NAN_BOXING
+        if (IS_BOOL(value)) {
             printf(AS_BOOL(value) ? "true" : "false");
-            break;
-        case VAL_NIL: printf("nil"); break;
-        case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
-        case VAL_OBJ: printObject(value); break;
-    };
+        } else if (IS_NIL(value)) {
+            printf("nil");
+        } else if (IS_NUMBER(value)) {
+            printf("%g", AS_NUMBER(value));
+        } else if (IS_OBJ(value)) {
+            printObject(value);
+        }
+    #else
+        switch (value.type) {
+            case VAL_BOOL:
+                printf(AS_BOOL(value) ? "true" : "false");
+                break;
+            case VAL_NIL: printf("nil"); break;
+            case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+            case VAL_OBJ: printObject(value); break;
+        };
+    #endif
 }
